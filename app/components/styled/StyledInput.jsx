ca68342cd0ba2c782590d21b8f483384ab899165
dastardlychimp <darien.hess@demochimp.com>
2017-06-20 18:21:52 -0600

    messing with styles

--- app/components/styled/StyledInput.jsx
+++ app/components/styled/StyledInput.jsx
@@ -1,12 +1,24 @@
-import styled from 'styled-components'
+import styled, {keyframes} from 'styled-components'
+
+const kf = keyframes`
+    from {
+        outline-color: black
+    }
+
+    to {
+        outline-color: rgb(111, 237, 178);
+    }
+`
 
 export default styled.input`
     height:  15px;
     width:   400px;
     padding: 10px;
     border: 1px solid black;
+    font-size: 14px;
 
     &:focus {
-        outline: 2px solid green;
+        outline: 2px solid rgb(111, 237, 178);
+        animation: ${kf} 0.5s linear;
     }
 `
